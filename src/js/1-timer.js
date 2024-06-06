3566902a398beac35c1ea2bb9e34bfc3eea1ddd1
Yana Ushakova <115149484+yana1982@users.noreply.github.com>
2024-06-06 14:47:02 +0300

    update package-lock.json file, package.json file,  1-timer.html file,  2-snackbar.html file,  1-timer.css file,  2-snackbar.css file,  1-timer.js file,  2-snackbar.js file,  add  styles.css file, error.svg file, success.svg file

---
+++ src/js/1-timer.js
@@ -0,0 +1,88 @@
+// flatpickr
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+// iziToast
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+// imageUrl
+import imageUrl from '../img/error.svg';
+
+let userSelectedDate;
+const dateTimePickerInput = document.querySelector('#datetime-picker');
+const startBtnElem = document.querySelector('[data-start]');
+const daysSpanElem = document.querySelector('[data-days]');
+const hoursSpanElem = document.querySelector('[data-hours]');
+const minutesSpanElem = document.querySelector('[data-minutes]');
+const secondsSpanElem = document.querySelector('[data-seconds]');
+startBtnElem.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = Date.parse(selectedDates[0]);
+    if (userSelectedDate < Date.now()) {
+      iziToast.error({
+        iconUrl: imageUrl,
+        message: 'Please choose a date in the future',
+        backgroundColor: '#ef4040',
+                       messageColor: '#fff',
+                       messageSize: '16',
+                       imageWidth: 302,
+                       close: true,
+                       closeOnEscape: true,
+                       closeOnClick: true,
+                       progressBar: true,
+                       progressBarColor: '#b51b1b',
+                       transitionIn: 'flipInX',
+                       transitionOut: 'flipOutX',
+                       position: 'topRight',
+                       iconColor: '#FAFAFB',
+      });
+      startBtnElem.disabled = true;
+    } else {
+      startBtnElem.disabled = false;
+    }
+  },
+};
+flatpickr(dateTimePickerInput, options);
+function startCountdown() {
+  startBtnElem.disabled = true;
+  dateTimePickerInput.disabled = true;
+  const intervalId = setInterval(() => {
+    let timeLeftMs = userSelectedDate - Date.now();
+    function convertMs(ms) {
+      
+      const second = 1000;
+      const minute = second * 60;
+      const hour = minute * 60;
+      const day = hour * 24;
+
+      
+      const days = Math.floor(ms / day);
+      
+      const hours = Math.floor((ms % day) / hour);
+     
+      const minutes = Math.floor(((ms % day) % hour) / minute);
+      
+      const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+      return { days, hours, minutes, seconds };
+    }
+    const timeLeft = convertMs(timeLeftMs);
+    function addLeadingZero(value) {
+      return String(value).padStart(2, '0');
+    }
+    daysSpanElem.textContent = addLeadingZero(timeLeft.days);
+    hoursSpanElem.textContent = addLeadingZero(timeLeft.hours);
+    minutesSpanElem.textContent = addLeadingZero(timeLeft.minutes);
+    secondsSpanElem.textContent = addLeadingZero(timeLeft.seconds);
+    if (timeLeftMs <= 1000) {
+      clearInterval(intervalId);
+      dateTimePickerInput.disabled = false;
+    }
+  }, 1000);
+}
+startBtnElem.addEventListener('click', startCountdown);
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,46 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import imageUrl from '../img/success.svg';
+import imageUrl1 from '../img/error.svg';
+
+const formElem = document.querySelector('.form');
+formElem.addEventListener('submit', event => {
+  event.preventDefault();
+  const delay = Number(formElem.delay.value);
+  const stateValue = formElem.state.value;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (stateValue === 'fulfilled') {
+        resolve(
+          iziToast.success({
+            iconUrl: imageUrl,
+            message: `Fulfilled promise in ${delay}ms`,
+            messageColor: '#ffffff',
+            backgroundColor: '#59A10D',
+            progressBar: true,
+            progressBarColor: '#326101',
+            progressBarEasing: 'linear',
+            pauseOnHover: true,
+            position: 'topRight',
+          })
+        );
+      } else {
+        reject(
+          iziToast.error({
+            iconUrl: imageUrl1,
+            message: `Rejected promise in ${delay}ms`,
+            messageColor: '#ffffff',
+            backgroundColor: '#EF4040',
+            progressBar: true,
+            progressBarColor: '#B51B1B',
+            progressBarEasing: 'linear',
+            pauseOnHover: true,
+            position: 'topRight',
+          })
+        );
+      }
+    }, delay);
+  });
+  formElem.reset();
+});
