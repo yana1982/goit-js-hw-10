2f3161d38d741078013f39dd238406d5d163b9b8
Yana Ushakova <115149484+yana1982@users.noreply.github.com>
2024-06-07 14:34:25 +0300

    update 1-timer.js file, 2-snackbar.js file

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -48,33 +48,16 @@ const options = {
   },
 };
 flatpickr(dateTimePickerInput, options);
+
 function startCountdown() {
   startBtnElem.disabled = true;
   dateTimePickerInput.disabled = true;
   const intervalId = setInterval(() => {
     let timeLeftMs = userSelectedDate - Date.now();
-    function convertMs(ms) {
-      
-      const second = 1000;
-      const minute = second * 60;
-      const hour = minute * 60;
-      const day = hour * 24;
-
-      
-      const days = Math.floor(ms / day);
-      
-      const hours = Math.floor((ms % day) / hour);
-     
-      const minutes = Math.floor(((ms % day) % hour) / minute);
-      
-      const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-      return { days, hours, minutes, seconds };
-    }
+    
+    
     const timeLeft = convertMs(timeLeftMs);
-    function addLeadingZero(value) {
-      return String(value).padStart(2, '0');
-    }
+    
     daysSpanElem.textContent = addLeadingZero(timeLeft.days);
     hoursSpanElem.textContent = addLeadingZero(timeLeft.hours);
     minutesSpanElem.textContent = addLeadingZero(timeLeft.minutes);
@@ -84,5 +67,26 @@ function startCountdown() {
       dateTimePickerInput.disabled = false;
     }
   }, 1000);
+}
+function convertMs(ms) {
+      
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  
+  const days = Math.floor(ms / day);
+  
+  const hours = Math.floor((ms % day) / hour);
+ 
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
 }
 startBtnElem.addEventListener('click', startCountdown);
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -7,40 +7,46 @@ import imageUrl1 from '../img/error.svg';
 const formElem = document.querySelector('.form');
 formElem.addEventListener('submit', event => {
   event.preventDefault();
+
   const delay = Number(formElem.delay.value);
   const stateValue = formElem.state.value;
+
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       if (stateValue === 'fulfilled') {
-        resolve(
-          iziToast.success({
-            iconUrl: imageUrl,
-            message: `Fulfilled promise in ${delay}ms`,
-            messageColor: '#ffffff',
-            backgroundColor: '#59A10D',
-            progressBar: true,
-            progressBarColor: '#326101',
-            progressBarEasing: 'linear',
-            pauseOnHover: true,
-            position: 'topRight',
-          })
-        );
-      } else {
-        reject(
-          iziToast.error({
-            iconUrl: imageUrl1,
-            message: `Rejected promise in ${delay}ms`,
-            messageColor: '#ffffff',
-            backgroundColor: '#EF4040',
-            progressBar: true,
-            progressBarColor: '#B51B1B',
-            progressBarEasing: 'linear',
-            pauseOnHover: true,
-            position: 'topRight',
-          })
-        );
+        resolve(delay);
       }
-    }, delay);
-  });
-  formElem.reset();
+       else {
+        reject(delay);
+      }
+      }, delay);
+    });
+
+    promise
+    .then((delay) => {
+      iziToast.success({
+        iconUrl: imageUrl,
+        message: `Fulfilled promise in ${delay}ms`,
+        messageColor: '#ffffff',
+        backgroundColor: '#59A10D',
+        progressBar: true,
+        progressBarColor: '#326101',
+        progressBarEasing: 'linear',
+        pauseOnHover: true,
+        position: 'topRight',
+    });
+  })
+    .catch((delay) => {
+      iziToast.error({
+        iconUrl: imageUrl1,
+        message: `Rejected promise in ${delay}ms`,
+        messageColor: '#ffffff',
+        backgroundColor: '#EF4040',
+        progressBar: true,
+        progressBarColor: '#B51B1B',
+        progressBarEasing: 'linear',
+        pauseOnHover: true,
+        position: 'topRight',
+      });
+    });
 });
